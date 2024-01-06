c611b114186d99bf998426e690c7559f55c661cc
zilanlann <666zilan666@gmail.com>
2024-01-06 14:12:43 +0800

    style: do code format

--- qlApi.js
+++ qlApi.js
@@ -7,7 +7,7 @@
  * @From: https://github.com/whyour/qinglong/issues/1369
  */
 const axios = require('axios')
-const QL_PATH = process.env.QL_PATH?process.env.QL_PATH:''
+const QL_PATH = process.env.QL_PATH ? process.env.QL_PATH : ''
 const QL_URL = `http://127.0.0.1:5700${QL_PATH}`
 const CLIENT_ID = process.env.CLIENT_ID
 const CLIENT_SECRET = process.env.CLIENT_SECRET
@@ -16,20 +16,20 @@ const CLIENT_SECRET = process.env.CLIENT_SECRET
  *获取青龙token
  */
 function getQLToken() {
-  return new Promise((resolve, reject) => {
-    axios
-      .get(
-        QL_URL +
-          `/open/auth/token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
-      )
-      .then(res => {
-        if (res.data.code === 200) {
-          resolve(res.data.data.token)
-        } else {
-          reject(res.data.message)
-        }
-      })
-  })
+    return new Promise((resolve, reject) => {
+        axios
+            .get(
+                QL_URL +
+                `/open/auth/token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+            )
+            .then(res => {
+                if (res.data.code === 200) {
+                    resolve(res.data.data.token)
+                } else {
+                    reject(res.data.message)
+                }
+            })
+    })
 }
 
 /**
@@ -37,17 +37,17 @@ function getQLToken() {
  * @returns headers
  */
 async function generateRequestHeader() {
-  return new Promise(async resolve => {
-    const token = await getQLToken()
-    resolve({
-      Authorization: 'Bearer ' + token,
-      'User-Agent':
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4577.63 Safari/537.36',
-      'Content-Type': 'application/json;charset=UTF-8',
-      'Accept-Encoding': 'gzip, deflate',
-      'Accept-Language': 'zh-CN,zh;q=0.9'
+    return new Promise(async resolve => {
+        const token = await getQLToken()
+        resolve({
+            Authorization: 'Bearer ' + token,
+            'User-Agent':
+                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4577.63 Safari/537.36',
+            'Content-Type': 'application/json;charset=UTF-8',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,zh;q=0.9'
+        })
     })
-  })
 }
 
 /**
@@ -55,18 +55,18 @@ async function generateRequestHeader() {
  * @returns axios instance
  */
 async function init() {
-  if (!CLIENT_ID || !CLIENT_SECRET)
-    return Promise.reject('未获取到 CLIENT_ID 或 CLIENT_SECRET')
-  const headers = await generateRequestHeader()
-  return new Promise(resolve => {
-    resolve(
-      axios.create({
-        baseURL: QL_URL,
-        timeout: 10000,
-        headers
-      })
-    )
-  })
+    if (!CLIENT_ID || !CLIENT_SECRET)
+        return Promise.reject('未获取到 CLIENT_ID 或 CLIENT_SECRET')
+    const headers = await generateRequestHeader()
+    return new Promise(resolve => {
+        resolve(
+            axios.create({
+                baseURL: QL_URL,
+                timeout: 10000,
+                headers
+            })
+        )
+    })
 }
 
 /**
@@ -76,18 +76,18 @@ async function init() {
  * @returns [] envlist
  */
 function getQLEnvs(instance, searchValue = 'JD_COOKIE') {
-  return new Promise(resolve => {
-    instance
-      .get('/open/envs', {
-        params: {
-          searchValue,
-          t: +new Date()
-        }
-      })
-      .then(res => {
-        resolve(res.data.data.filter(v => v.status === 0))
-      })
-  })
+    return new Promise(resolve => {
+        instance
+            .get('/open/envs', {
+                params: {
+                    searchValue,
+                    t: +new Date()
+                }
+            })
+            .then(res => {
+                resolve(res.data.data.filter(v => v.status === 0))
+            })
+    })
 }
 
 /**
@@ -97,16 +97,16 @@ function getQLEnvs(instance, searchValue = 'JD_COOKIE') {
  * @returns
  */
 function createCkEnv(instance, ck = []) {
-  return new Promise(resolve => {
-    instance
-      .post(`/open/envs?t=${+new Date()}`, ck)
-      .then(res => {
-        resolve(res.data)
-      })
-      .catch(error => {
-        console.log(error.response.data)
-      })
-  })
+    return new Promise(resolve => {
+        instance
+            .post(`/open/envs?t=${+new Date()}`, ck)
+            .then(res => {
+                resolve(res.data)
+            })
+            .catch(error => {
+                console.log(error.response.data)
+            })
+    })
 }
 
 /**
@@ -116,16 +116,16 @@ function createCkEnv(instance, ck = []) {
  * @returns
  */
 function updateCkEnv(instance, ck = {}) {
-  return new Promise(resolve => {
-    instance
-      .put(`/open/envs?t=${+new Date()}`, ck)
-      .then(res => {
-        resolve(res.data)
-      })
-      .catch(error => {
-        console.log(error.response.data)
-      })
-  })
+    return new Promise(resolve => {
+        instance
+            .put(`/open/envs?t=${+new Date()}`, ck)
+            .then(res => {
+                resolve(res.data)
+            })
+            .catch(error => {
+                console.log(error.response.data)
+            })
+    })
 }
 
 /**
@@ -135,13 +135,13 @@ function updateCkEnv(instance, ck = {}) {
  * @returns
  */
 function deleteCkEnv(instance, ckIds = []) {
-  return new Promise(resolve => {
-    instance({
-      method: 'delete',
-      url: `/open/envs?t=${+new Date()}`,
-      data: ckIds
-    }).then(resolve)
-  })
+    return new Promise(resolve => {
+        instance({
+            method: 'delete',
+            url: `/open/envs?t=${+new Date()}`,
+            data: ckIds
+        }).then(resolve)
+    })
 }
 
 /**
@@ -152,11 +152,11 @@ function deleteCkEnv(instance, ckIds = []) {
  * @returns
  */
 function toggleCKEnv(instance, id, path = 'enable') {
-  return new Promise(resolve => {
-    instance.put(`/open/envs/${path}?t=${+new Date()}`, [id]).then(res => {
-      resolve(res.data)
+    return new Promise(resolve => {
+        instance.put(`/open/envs/${path}?t=${+new Date()}`, [id]).then(res => {
+            resolve(res.data)
+        })
     })
-  })
 }
 
 exports.createEnv = createCkEnv
